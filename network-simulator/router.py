a2daa1168b8fd929956720bb3aa8f07ee659ff99
EC2 Default User <ec2-user@ip-172-31-25-39.us-west-2.compute.internal>
2016-02-12 23:27:28 +0000

    Read a file (bytewise)

--- network-simulator/router.py
+++ network-simulator/router.py
@@ -1,3 +1,15 @@
-def hello_world():
-	print 'Hello World !'
-    return 1
+#!/usr/bin/env python
+
+from sys import argv
+
+script, in_file = argv
+
+# Packet Size (in bytes)
+piece_size = 512
+
+ifile = open(in_file,'rb')
+data = ifile.read(piece_size)
+while data:
+    print data
+    data = ifile.read(piece_size)
+ifile.close()
