104031643b1da099cc8b4b3a65b9a619590bcbae
jugg3rn4u7 <shriku4u27@gmail.com>
2016-02-22 00:19:48 +0000

    PacketParser code

--- network-simulator/router1.py
+++ network-simulator/router1.py
@@ -5,11 +5,12 @@ import queue
 import time
 import threading
 from threading import Thread
+from struct import *
 
 script, in_file1, in_file2, in_file3 = argv
 
 # Packet Size (in bytes)
-packet_size = 50
+packet_size = 500
 
 # Define a Queue
 queueList = [ queue.Queue() for i in range(3) ]
@@ -19,6 +20,7 @@ delay1 = 1
 delay2 = 3
 delay3 = 2
 
+# Multithreading - Data Links receive data in parallel
 class RouterThread (Thread):
     def __init__(self, threadID, name, in_file, delay, queue):
         "Init code for Thread class"
@@ -71,10 +73,106 @@ def data_link( self, threadName, delay, in_file, queue):
     ifile.close()
     return
 
+class Packet(object):
+    def __init__(self):
+        "Init for Packet Object"
+        self._version = ""
+        self._ihl = ""
+        self._dscp__ = ""
+        self._ecn__ = ""
+        self._totalLength = 0
 
-class ReceiverModule:
+    @property
+    def version(self):
+        return self._version
+
+    @version.setter
+    def version(self, value):
+        self._version = value
+
+    @version.deleter
+    def version(self):
+        del self._version
+
+    @property
+    def ihl(self):
+        return self._ihl
+
+    @ihl.setter
+    def ihl(self, value):
+        self._ihl = value
+
+    @ihl.deleter
+    def ihl(self):
+        del self._ihl
+
+    @property
+    def dscp(self):
+        return self._dscp
+
+    @dscp.setter
+    def dscp(self, value):
+        self._dscp = value
+
+    @dscp.deleter
+    def dscp(self):
+        del self._dscp
+
+    @property
+    def ecn(self):
+        return self._ecn
+
+    @ecn.setter
+    def ecn(self, value):
+        self._ecn = value
+
+    @ecn.deleter
+    def ecn(self):
+        del self._ecn
+
+    @property
+    def totalLength(self):
+        return self._totalLength
+
+    @totalLength.setter
+    def totalLength(self, value):
+        self._totalLength = value
+
+    @ihl.deleter
+    def totalLength(self):
+        del self._totalLength
+
+
+def eth_addr (a) :
+  b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (a[0] , a[1] , a[2], a[3], a[4] , a[5])
+  return b
+
+
+# Packet Parser - Parser that parses string data and gives datagram object
+# Takes Input as protocol, Outputs packet object
+class PacketParser(Packet):
+    def __init__( self ):
+        "Initialization method for Packet Parser"
+        
+    def parsePacket( self , data ):
+        "Parse a Packet"
+
+        #print("Packet Data in string : " + binascii.unhexlify(data[1:60]))
+        #print(data[96:127].decode("utf-8"))
+
+        #parse ethernet header
+        eth_length = 14
+     
+        eth_header = data[:eth_length]
+        eth = unpack('!6s6sH' , eth_header)
+        
+        print('Destination MAC : ' + eth_addr(data[0:6]) + ' Source MAC : ' + eth_addr(data[6:12]))
+
+# Receiver Module - Takes data from data links
+class ReceiverModule(PacketParser):
     def __init__(self):
         "Init method for the class"
+        self.PacketParser = PacketParser
         self.threadLock = threading.Lock()
         self.threads = []
 
@@ -100,23 +198,26 @@ class ReceiverModule:
             t.join()
         print("Exiting Main Thread")
 
+        packetParser = self.PacketParser()
+
         # Print output - Get bytes from Queue
         print("Printing items from each queue -");
         for queue in queueList:
             while not queue.empty():
-                print(queue.get().decode("utf-8"))
+                print("***********************************************************************************************************")
+                packetParser.parsePacket( queue.get() )
 
+# Router
+class Router(ReceiverModule):
+    def __init__( self ):
+        "Init method"
+        return
+    
+    def start( self ):
+        "Start method"
+        self.receiverModule = ReceiverModule()
+        self.receiverModule.start();
+        return
 
-class Router:
-    def __init__(self):
-        "Initialization method for Router"
-        # Currently do nothing
-
-    def start(self):
-        "Start method for the router"
-        receiverModule = ReceiverModule()
-        receiverModule.start()
-
-        
 router = Router()
 router.start()
