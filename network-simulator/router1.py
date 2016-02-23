2748c8157c5ffbc4e2e3db3e228a9bc304d0d891
jugg3rn4u7 <shriku4u27@gmail.com>
2016-02-23 04:57:08 +0000

    Packet Parser, IP Formatter

--- network-simulator/router1.py
+++ network-simulator/router1.py
@@ -6,6 +6,7 @@ import time
 import threading
 from threading import Thread
 from struct import *
+import binascii
 
 script, in_file1, in_file2, in_file3 = argv
 
@@ -80,7 +81,16 @@ class Packet(object):
         self._ihl = ""
         self._dscp__ = ""
         self._ecn__ = ""
-        self._totalLength = 0
+        self._totalLength = ""
+        self._identification = ""
+        self._flags = ""
+        self._fragmentOffset = ""
+        self._timeToLive = ""
+        self._protocol = ""
+        self._headerChecksum = ""
+        self._sourceAddress = ""
+        self._destinationAddress = ""
+        self._options = ""
 
     @property
     def version(self):
@@ -138,15 +148,145 @@ class Packet(object):
     def totalLength(self, value):
         self._totalLength = value
 
-    @ihl.deleter
+    @totalLength.deleter
     def totalLength(self):
         del self._totalLength
 
+    @property
+    def identification(self):
+        return self._identification
 
-def eth_addr (a) :
-  b = "%.2x:%.2x:%.2x:%.2x:%.2x:%.2x" % (a[0] , a[1] , a[2], a[3], a[4] , a[5])
-  return b
+    @identification.setter
+    def identification(self, value):
+        self._identification = value
 
+    @identification.deleter
+    def identification(self):
+        del self._identification
+
+    @property
+    def flags(self):
+        return self._flags
+
+    @flags.setter
+    def flags(self, value):
+        self._flags = value
+
+    @flags.deleter
+    def flags(self):
+        del self._flags
+
+    @property
+    def fragmentOffset(self):
+        return self._fragmentOffset
+
+    @fragmentOffset.setter
+    def fragmentOffset(self, value):
+        self._fragmentOffset = value
+
+    @fragmentOffset.deleter
+    def fragmentOffset(self):
+        del self._fragmentOffset
+
+    @property
+    def timeToLive(self):
+        return self._timeToLive
+
+    @timeToLive.setter
+    def timeToLive(self, value):
+        self._timeToLive = value
+
+    @timeToLive.deleter
+    def timeToLive(self):
+        del self._timeToLive
+
+    @property
+    def protocol(self):
+        return self._protocol
+
+    @protocol.setter
+    def protocol(self, value):
+        self._protocol = value
+
+    @protocol.deleter
+    def protocol(self):
+        del self._protocol
+
+    @property
+    def headerChecksum(self):
+        return self._headerChecksum
+
+    @headerChecksum.setter
+    def headerChecksum(self, value):
+        self._headerChecksum = value
+
+    @headerChecksum.deleter
+    def headerChecksum(self):
+        del self._headerChecksum
+
+    @property
+    def sourceAddress(self):
+        return self._sourceAddress
+
+    @sourceAddress.setter
+    def sourceAddress(self, value):
+        self._sourceAddress = value
+
+    @sourceAddress.deleter
+    def sourceAddress(self):
+        del self._sourceAddress
+
+    @property
+    def destinationAddress(self):
+        return self._destinationAddress
+
+    @destinationAddress.setter
+    def destinationAddress(self, value):
+        self._destinationAddress = value
+
+    @destinationAddress.deleter
+    def destinationAddress(self):
+        del self._destinationAddress
+
+    @property
+    def options(self):
+        return self._options
+
+    @options.setter
+    def options(self, value):
+        self._options = value
+
+    @options.deleter
+    def options(self):
+        del self._options 
+
+def ByteToHex( byteStr ):
+    return [ "%02X " % ord( x ) for x in byteStr ]
+
+def HexToBin( hexArr ):
+    scale = 16 ## equals to hexadecimal
+    num_of_bits = 8
+
+    return ''.join( [  bin( int( x , scale ) )[2:].zfill( num_of_bits ) for x in hexArr ] ).strip()
+
+def BinToInt( binStr  ):
+    return int( binStr, 2 )
+
+def DecToHex( decimal ):
+    return hex( decimal )
+
+def HexToDec( hexadecimal ):
+    return int( hexadecimal, 16 )
+
+def IPFormatter( decimal ):
+    h1 = DecToHex( decimal )
+    h2 = h1.split('x')[1]
+    octet1 = HexToDec( '0x' + h2[0:2] )
+    octet2 = HexToDec( '0x' + h2[2:4] )
+    octet3 = HexToDec( '0x' + h2[4:6] )
+    octet4 = HexToDec( '0x' + h2[6:8] )
+    ip = str(octet1) + '.' + str(octet2) + '.' + str(octet3) + '.' + str(octet4)
+    return ip
 
 # Packet Parser - Parser that parses string data and gives datagram object
 # Takes Input as protocol, Outputs packet object
@@ -156,17 +296,35 @@ class PacketParser(Packet):
         
     def parsePacket( self , data ):
         "Parse a Packet"
-
-        #print("Packet Data in string : " + binascii.unhexlify(data[1:60]))
-        #print(data[96:127].decode("utf-8"))
-
-        #parse ethernet header
-        eth_length = 14
-     
-        eth_header = data[:eth_length]
-        eth = unpack('!6s6sH' , eth_header)
         
-        print('Destination MAC : ' + eth_addr(data[0:6]) + ' Source MAC : ' + eth_addr(data[6:12]))
+        packet = Packet()
+
+        sliced_str = str(data[:60])[2:-1]
+
+        #print(sliced_str)
+
+        pkt_str = HexToBin(ByteToHex(sliced_str))
+    
+        #print(pkt_str[16:32])
+        
+
+        ## IP4 Datagram structure
+        packet.version = BinToInt( pkt_str[0:4] )
+        packet.ihl = BinToInt( pkt_str[4:8] )
+        packet.dscp = BinToInt( pkt_str[8:14] )
+        packet.ecn = BinToInt( pkt_str[14:16] )
+        packet.totalLength = BinToInt( pkt_str[16:32]  )
+        packet.identification = BinToInt( pkt_str[32:48] )
+        packet.flags = BinToInt( pkt_str[64:66] )
+        packet.fragmentOffset = BinToInt( pkt_str[66:80] )
+        packet.timeToLive = BinToInt( pkt_str[80:88] )
+        packet.protocol = BinToInt( pkt_str[88:104] )
+        packet.headerChecksum = BinToInt( pkt_str[104:130] )
+        packet.sourceAddress = BinToInt( pkt_str[130:162] )
+        packet.destinationAddress = BinToInt( pkt_str[162:194] )
+        packet.options = BinToInt( pkt_str[194:226] )
+
+        print('Version : ' + str(packet.version) + ' source address : ' + IPFormatter(packet.sourceAddress) + ' destination address : ' + IPFormatter(packet.destinationAddress))
 
 # Receiver Module - Takes data from data links
 class ReceiverModule(PacketParser):
